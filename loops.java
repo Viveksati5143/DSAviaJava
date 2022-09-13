64633912dde670446b0157c5b88a0d0750575997
Vivek Sati <viveksatipoco@gmail.com>
2022-09-13 23:42:14 +0530

    first commit

--- loops.java
+++ loops.java
@@ -1,5 +1,12 @@
 package dsa;
 
-public class loops {
-
+public class Loops {
+	public static void main(String[] args) {
+		int a = 1;
+		while(a <=7) {
+			System.out.println(a);
+			a++ ;
+		}
+		System.out.print("Done");				
+	}
 }
