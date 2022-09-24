978d3962b92d5a9c70dc6d398d1b4896e3ed5fe4
Vivek Sati <viveksatipoco@gmail.com>
2022-09-24 20:06:12 +0530

    Fourth commit

---
+++ Count_Digits.java
@@ -0,0 +1,17 @@
+package dsa;
+import java.util.*;
+
+public class Count_Digits {
+  
+	  public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int d=0;
+	    while(n!=0){
+	      n=n/10;
+	      d++;
+	    }
+	    System.out.println(d);
+
+	    }
+}
